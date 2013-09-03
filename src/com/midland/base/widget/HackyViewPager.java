aee020852e2449cdcd04d3630b228b2594c75e17
Roy Ng <royng@midland.com.hk>
2013-09-03 12:26:29 +0800

    Add HackyViewPager

---
+++ src/com/midland/base/widget/HackyViewPager.java
@@ -0,0 +1,30 @@
+package com.midland.base.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.midland.base.util.Ran;
+
+public class HackyViewPager extends ViewPager {
+
+    public HackyViewPager(Context context) {
+        super(context);
+    }
+
+    public HackyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}

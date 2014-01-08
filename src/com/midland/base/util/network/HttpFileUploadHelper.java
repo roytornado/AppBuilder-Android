82c61ee75c48bf5ab363f9f2f23aebd8515d6114
Boster Cheng <bosterc@midland.com.hk>
2014-01-08 16:17:14 +0800

    Add Log about InputStream DataOS and Total Time Taken

--- src/com/midland/base/util/network/HttpFileUploadHelper.java
+++ src/com/midland/base/util/network/HttpFileUploadHelper.java
@@ -37,9 +37,16 @@ public class HttpFileUploadHelper {
     }
 
     public void performUpload(final String urlStr, final Map<String, String> params, final Uri fileUri) {
+        long startDataOS = 0;
+        long endDataOS = 0;
+        long startInputStream = 0;
+        long endInputStream = 0;
+        long startUploadTask = 0;
+        long endUploadTask = 0;
         Message message = handler.obtainMessage();
         Bundle bundle = new Bundle();
         try {
+            startUploadTask = System.currentTimeMillis();
             URL url = new URL(urlStr);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
@@ -48,9 +55,10 @@ public class HttpFileUploadHelper {
             conn.setUseCaches(false);
             conn.setConnectTimeout(300000);
             conn.setReadTimeout(300000);
-            conn.setChunkedStreamingMode(262144);
-            conn.setRequestProperty("Connection","Keep-Alive");
+            //conn.setChunkedStreamingMode(262144);
+            //conn.setRequestProperty("Connection", "Keep-Alive");
             conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+            startDataOS = System.currentTimeMillis();
             DataOutputStream dataOS = new DataOutputStream(conn.getOutputStream());
             dataOS.writeBytes(HYPHENS + BOUNDARY + CRLF);
             // ******************************************** HANDLE PARAMS
@@ -73,13 +81,15 @@ public class HttpFileUploadHelper {
                 dataOS.writeBytes("Content-Type: " + file_type + CRLF);
                 dataOS.writeBytes(CRLF);
 
-             //   int iBytesAvailable = fileInputStream.available();
-              //  byte[] byteData = new byte[iBytesAvailable];
+                //   int iBytesAvailable = fileInputStream.available();
+                //  byte[] byteData = new byte[iBytesAvailable];
+                int currentByte = 0;
                 int count;
                 byte buffer[] = new byte[1024];
-                while ((count = fileInputStream.read(buffer)) > 0)
-                {
+                while ((count = fileInputStream.read(buffer)) > 0) {
                     dataOS.write(buffer, 0, count);
+                    currentByte += count;
+                    Common.d("HttpFileUploadHelper writing KByte: " + currentByte / 1024);
                 }
                /* int iBytesRead = fileInputStream.read(byteData, 0, iBytesAvailable);
                 while (iBytesRead > 0) {
@@ -90,12 +100,15 @@ public class HttpFileUploadHelper {
                 dataOS.writeBytes(CRLF);
                 dataOS.writeBytes(HYPHENS + BOUNDARY + HYPHENS + CRLF);
                 fileInputStream.close();
+
             }
             // ******************************************** END
-
+            Common.d("HttpFileUploadHelper dataOS Finish");
+            endDataOS = System.currentTimeMillis();
             dataOS.flush();
             dataOS.close();
-
+            Common.d("HttpFileUploadHelper InputStream Start");
+            startInputStream = System.currentTimeMillis();
             InputStream responseStream = new BufferedInputStream(conn.getInputStream());
             BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
             String line = "";
@@ -104,11 +117,16 @@ public class HttpFileUploadHelper {
                 stringBuilder.append(line).append("\n");
             }
             responseStreamReader.close();
-
+            endInputStream = System.currentTimeMillis();
+            endUploadTask = System.currentTimeMillis();
+            Common.d("HttpFileUploadHelper InputStream Finish");
+            Common.d("HttpFileUploadHelper DataOS Time Taken" + (endDataOS - startDataOS));
+            Common.d("HttpFileUploadHelper InputStream Time Taken" + (endInputStream - startInputStream));
+            Common.d("HttpFileUploadHelper Total Time Taken" + (endUploadTask - startUploadTask));
             String response = stringBuilder.toString();
             String code = conn.getResponseCode() + "";
-            Common.d("CODE: "+code);
-            Common.d("RESPONSE: "+response);
+            Common.d("CODE: " + code);
+            Common.d("RESPONSE: " + response);
             responseStream.close();
             conn.disconnect();
             bundle.putString("CODE", code);

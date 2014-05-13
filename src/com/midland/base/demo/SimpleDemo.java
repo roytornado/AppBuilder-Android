94f5b7be61c5b0517c401d275be115743d0cbbbb
Roy Ng <roytornado@gmail.com>
2014-05-13 11:25:15 +0800

    Revise

--- src/com/midland/base/demo/SimpleDemo.java
+++ src/com/midland/base/demo/SimpleDemo.java
@@ -5,6 +5,10 @@ import android.widget.ImageView;
 import com.midland.base.R;
 import com.midland.base.app.BaseApp;
 import com.midland.base.util.ImageLoader;
+import com.midland.base.util.network.ServerTaskListener;
+import com.midland.base.util.network.ServerTaskManager;
+
+import java.util.HashMap;
 
 /**
  * Created by royng on 17/4/14.
@@ -12,10 +16,29 @@ import com.midland.base.util.ImageLoader;
 public class SimpleDemo {
 
 
+    ServerTaskManager stm;
     public void demoLoadImages() {
         String url = "http://image.com/1234";
         ImageView imageView = new ImageView(BaseApp.me);
         ImageLoader.getLoader().loadImgOtherThread(url,imageView, ImageLoader.ImgSizeType.THUMB, R.drawable.ic_image);
+
+
+        stm.startTask("Host",new HashMap<String, String>(),new ServerTaskListener() {
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onSuccess(String result) {
+
+            }
+
+            @Override
+            public void onError(String result, String code, String msg) {
+
+            }
+        });
 
     }
 }
